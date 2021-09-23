9e7002619e2afe43e642c789ff34111d5a1fe23a
Nishu Tyagi <tyaginishu7@gmail.com>
2021-09-23 20:08:26 +0530

    [Nishu Tyagi]Added:JS Program to check Prototype and Hoisting-flow of JS

---
+++ hoisting.js
@@ -0,0 +1,13 @@
+console.log(a);
+var a = 2;
+
+// console.log(y);
+function sum() {
+    console.log(a+b);
+    let b = 4;
+    let a = 1;
+}
+console.log(sum);
+// sum();
+
+console.log(y);
---
+++ prototype.js
@@ -0,0 +1,14 @@
+let details = {
+    name: "Nishu",
+    email: "nistyagi@gmail.com",
+    phoneno: 9877767761,
+    address: "Vasundhara, Ghaziabad"
+}
+
+let x = func(2, 8);
+function func(a, b) {
+    console.log(a+b);
+}
+
+var arr = ["Hakka Noodles", "Pasta", "Pizza"]
+
